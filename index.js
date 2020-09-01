bf0904ee136eb3561fbf072e2a999507c222a686
PriyankVIT <priyankkaushik13@gmail.com>
2020-09-02 03:51:23 +0530

    Try Again

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 
-gapi.load('auth2', function () {
-    auth2.signIn().then(function () {
-        console.log(auth2.currentUser.get().getId());
+    gapi.load('auth2', function () {
+        var auth2 = gapi.auth2.init();
+        auth2.signIn().then(function () {
+            console.log(auth2.currentUser.get().getId());
+        });
     });
-});
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('Full Name: ' + profile.getName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-}
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
+    function onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('Full Name: ' + profile.getName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+    }
+    function signOut() {
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
         console.log('User signed out.');
-    });
-}
+        });
+    }
 
-var auth2 = gapi.auth2.init();
+    const database = firebase.database();
+
+    const addBtn = document.getElementById('addBtn');
+
+    addBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        database.ref('/users/'+ profile.getName().value).set({
+            name: profile.getName().value
+        })
+    });
