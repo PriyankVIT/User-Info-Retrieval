a15999880a285dc2eadbce34f3d5be4d69aef43e
PriyankVIT <priyankkaushik13@gmail.com>
2020-09-03 16:13:15 +0530

    retrieve 3

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function onSignIn(googleUser) {
     ref.on("value", function(snapshot) {
         snapshot.forEach(function(childSnapshot) {
             var data = childSnapshot.val()
-            console.log(data.name);
+            console.log(data.Name);
         })
     })
 }
